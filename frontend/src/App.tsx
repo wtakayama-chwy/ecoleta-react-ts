9bd885428844e524b0743d9fd8276083bc6a45ef
William S. Takayama <william-sobreira@Hotmail.com>
2020-06-08 11:21:30 -0300

    frontend with react typescript done

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.scss';
+import Routes from './routes';
+
+function App() {
+  
+  return (
+    <>
+      <Routes />
+    </>
+  );
+}
+
+export default App;
