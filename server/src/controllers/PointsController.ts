220ec0438d6534222ca2e088c6ee952428cdc810
William S. Takayama <william-sobreira@Hotmail.com>
2020-06-10 19:15:24 -0300

    Added upload image feature on web

--- server/src/controllers/PointsController.ts
+++ server/src/controllers/PointsController.ts
@@ -17,7 +17,14 @@ class PointsController {
             .distinct()
             .select('points.*');
 
-        return res.json(points);
+        const serializedPoints = points.map(point => {
+            return {
+                ...point,
+                image_url: `http://192.168.0.103:3333/uploads/${point.image}`
+            };
+        })
+
+        return res.json(serializedPoints);
     }
 
     // Specific - req.params
@@ -32,8 +39,13 @@ class PointsController {
             .join('point_items', 'items.id', '=', 'point_items.item_id')
             .where('point_items.point_id', id)
             .select('items.title');
+
+        const serializedPoint = {
+            ...point,
+            image_url: `http://192.168.0.103:3333/uploads/${point.image}`
+        };
         
-        return res.json({ point, items });
+        return res.json({ point: serializedPoint, items });
     }
 
     // Creating = req.body
@@ -54,7 +66,7 @@ class PointsController {
          */
         const trx = await knex.transaction();
         const point = {
-            image: 'https://images.unsplash.com/photo-1542838132-92c53300491e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60',
+            image: req.file.filename,
             name,
             email,
             whatsapp,
@@ -67,12 +79,15 @@ class PointsController {
         const insertedIds = await trx('points').insert(point);
 
         const point_id = insertedIds[0];
-        const pointItems = items.map((item_id: number) => {
-            return {
-                item_id,
-                point_id
-            }
-        })
+        const pointItems = items
+            .split(',')
+            .map((item: string) => Number(item.trim()))
+            .map((item_id: number) => {
+                return {
+                    item_id,
+                    point_id
+                }
+            })
 
         await trx('point_items').insert(pointItems);
         await trx.commit(); // IMPORTANT TO INSERT ON TABLE
